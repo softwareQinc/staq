6218940460d682d40b35a6ece4b6c9571eedbcb6
Matt Amy <matt.e.amy@gmail.com>
2019-11-18 15:06:37 -0400

    Added benchmark circuits + change to run qiskit

--- demos/run_qiskit.py
+++ demos/run_qiskit.py
@@ -4,6 +4,8 @@ from qiskit.test.mock import FakeTokyo
 
 import sys
 
+basis = ['t', 'tdg', 's', 'sdg', 'cx', 'x', 'y', 'z', 'h']
+
 circ = QuantumCircuit.from_qasm_file(sys.argv[1])
 print("Original:\n  {}".format(circ.count_ops()))
 
@@ -12,6 +14,6 @@ circ1 = transpile(circ, backend=None, seed_transpiler=11, optimization_level=3)
 print("Optimized:\n  {}".format(circ1.count_ops()))
 
 # Optimize and map
-circ2 = transpile(circ, backend=FakeTokyo(), seed_transpiler=11, optimization_level=3)
+circ2 = transpile(circ, basis_gates=basis, backend=FakeTokyo(), seed_transpiler=11, optimization_level=3)
 circ2.count_ops()
 print("Mapped:\n  {}".format(circ2.count_ops()))
