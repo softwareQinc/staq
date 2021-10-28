84fc372b05431bd4a49bd76607ca7d033894b702
Scott Xu <scottx@untether.ai>
2021-10-28 09:23:42 -0400

    pip installation

---
+++ setup.py
@@ -0,0 +1,22 @@
+from glob import glob
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension
+
+ext_modules = [
+    Pybind11Extension(
+        "pystaq",
+        ["pystaq/staq_wrapper.cpp"],
+        extra_compile_args=["-Ilibs", "-Iinclude", "-Iqasmtools/include"],
+        cxx_std=17,
+        include_pybind11=False,
+    ),
+]
+
+setup(
+    name='pystaq',
+    version='2.0',
+    description='Python wrapper for staq',
+    author='softwareQ',
+    author_email='info@softwareq.ca',
+    url='https://github.com/softwareQinc/staq',
+    ext_modules=ext_modules)
