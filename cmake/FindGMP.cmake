2e1d79010b75556803bea8b805da816784c2ce83
Vlad Gheorghiu <vsoftco@gmail.com>
2023-08-16 14:11:15 -0400

    update

--- cmake/FindGMP.cmake
+++ cmake/FindGMP.cmake
@@ -1,15 +1,11 @@
 set(GMP_PREFIX "" CACHE PATH "path ")
 
-
 find_path(GMP_INCLUDE_DIR gmp.h gmpxx.h 
     PATHS ${GMP_PREFIX}/include /usr/include /usr/local/include
     /opt/homebrew/include)
 
 find_library(GMP_LIBRARY NAMES gmp libgmp 
     PATHS ${GMP_PREFIX}/lib /usr/lib /usr/local/lib)
-
-
-
 
 if(GMP_INCLUDE_DIR AND GMP_LIBRARY)
     get_filename_component(GMP_LIBRARY_DIR ${GMP_LIBRARY} PATH)
