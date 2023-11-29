ea0389d7081f7504ed8b3400293c3e924c0a6c90
Vlad Gheorghiu <vsoftco@gmail.com>
2023-11-29 12:53:37 -0500

    update

--- cmake/staq_msvc.cmake
+++ cmake/staq_msvc.cmake
@@ -1,9 +1,7 @@
 #### Windows issues with Microsoft Visual Studio
 if (MSVC)
-    message(STATUS "MSVC -- including the cmake")
     include_directories(SYSTEM
             ${STAQ_INSTALL_DIR}/third_party/pthreadwin32)
-    message(WARNING ${STAQ_INSTALL_DIR})
     add_compile_options(-bigobj)
     add_compile_definitions(NOMINMAX)
     if (MSVC_VERSION GREATER_EQUAL 1914)
