02d0007b8c99a37f0d2c2ce370561bc339a9e297
Vlad Gheorghiu <vsoftco@gmail.com>
2023-11-29 11:25:18 -0500

    update

--- cmake/staq_msvc.cmake
+++ cmake/staq_msvc.cmake
@@ -3,7 +3,6 @@ if (MSVC)
     message(STATUS "MSVC -- including the cmake")
     include_directories(SYSTEM
             ${CMAKE_INSTALL_PREFIX}/include/staq/third_party/pthreadwin32)
-    message(WARNING ${CMAKE_INSTALL_PREFIX})
     message(WARNING ${STAQ_INSTALL_DIR})
     add_compile_options(-bigobj)
     add_compile_definitions(NOMINMAX)
